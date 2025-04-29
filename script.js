0d2fb625539fd8dc473e05bd558b91a9c67c2879
bitfox79 <bitfox79@gmail.com>
2025-04-29 15:35:40 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function copyContractAddress() {
-    const contractAddress = "0x1234567890abcdef1234567890abcdef12345678";
-    navigator.clipboard.writeText(contractAddress);
-    alert("Адрес контракта скопирован: " + contractAddress);
-}
